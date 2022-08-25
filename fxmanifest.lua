1a5a4846e6d0be9eb8836739b33ebb107d1b4509
0xLuca <luczies@gmail.com>
2022-08-25 07:47:50 +0200

    first commit

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author '0xLuca'
+description 'Template resource'
+version '1.0.0'
+
+lua54 'yes'
+
+client_script 'client/**.lua'
+server_script 'server/**.lua'
+server_script 'server_config.lua'
+shared_script 'shared/**.lua'
+shared_script 'config.lua'
